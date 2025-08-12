577732706048d2e4e87502eb8012661d5c1649c8
Sherif <sherif871999@gmail.com>
2025-08-12 20:13:42 +0400

    Initial commit

---
+++ src/test/java/tests/ExtentReportListener.java
@@ -0,0 +1,75 @@
+package utils;
+
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.MediaEntityBuilder;  // مهم عشان تحط الصور في التقرير
+import org.testng.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import tests.BaseTest;
+
+
+public class ExtentReportListener implements ITestListener {
+
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentSparkReporter spark = new ExtentSparkReporter("reports/ExtentReport.html");
+        spark.config().setDocumentTitle("Automation Test Report");
+        spark.config().setReportName("Demo WebShop Tests");
+
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+        extent.setSystemInfo("Tester", "sherif");
+        extent.setSystemInfo("Environment", "QA");
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        String screenshotPath = takeScreenshot(result.getMethod().getMethodName());
+        if (screenshotPath != null) {
+            test.get().fail(result.getThrowable(),
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        } else {
+            test.get().fail(result.getThrowable());
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+    private String takeScreenshot(String testName) {
+        try {
+            File srcFile = ((TakesScreenshot) BaseTest.driver).getScreenshotAs(OutputType.FILE);
+            String path = "reports/screenshots/" + testName + "_" + System.currentTimeMillis() + ".png";
+            File destFile = new File(path);
+            FileUtils.copyFile(srcFile, destFile);
+            return path;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+}
+    }
+}
